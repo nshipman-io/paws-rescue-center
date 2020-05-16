960787c86a8166fc304b449d9fc4fc3cce6d0127
Norman Shipman <norman@nshipman.io>
2020-05-16 19:30:11 -0400

    Added session handling

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template, abort
-app = Flask(__name__)
+from flask import Flask, render_template, abort, session, redirect, url_for
+from forms import SignUpForm, SignInForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "KfbFLD5f545p5Vo5FANe4jyV"
 pets = [
         {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
         {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
         {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
         {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
     ]
+
+users = [
+         {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.com", "password": "adminpass"},
+    ]
+
 @app.route("/")
 def home():
+    if 'USERNAME' in session:
+        return render_template("home.html",pets=pets,user=session["USERNAME"])
+
     return render_template("home.html",pets=pets)
     
 @app.route("/about")
 def about():
+    if 'USERNAME' in session:
+        return render_template("about.html",pets=pets,user=session["USERNAME"])
     return render_template("about.html")
 
 @app.route("/details/<int:pet_id>")
@@ -21,7 +33,54 @@ def pet_details(pet_id):
     if pet is None:
         abort(404, description="No pet with given ID found.")
 
+    if 'USERNAME' in session:
+        return render_template("details.html",pet=pet,user=session["USERNAME"])
     return render_template("details.html",pet=pet)
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print("Form submitted and Valid")
+        user = {"id":len(users)+1, "full_name":form.name.data, "email":form.email.data, "password":form.password.data}
+        users.append(user)
+        print(f'Adding user: {user["full_name"]} to database.')
+        return render_template("signup.html", message="Successfully signed up")
+    elif form.errors:
+        print(form.errors.items())
+        print(form.name.errors)
+        print(form.email.errors)
+        print(form.password.errors)
+        print(form.confirm.errors)
+    return render_template("signup.html",form=form)
+
+@app.route("/signin",methods=["GET","POST"])
+def signin():
+    form = SignInForm()
+    if form.validate_on_submit():
+        session_user = None
+        i = 0
+        while session_user is None and i < len(users):
+            user = users[i]
+            if form.email.data == user["email"]:
+                session_user = user
+            else:
+                i+=1
+        if session_user is None:
+            return render_template("signin.html",message="Email not found",form=form)
+        if form.password.data != session_user["password"]:
+            print(f'Wrong password entered for user: {session_user["email"]}')
+            return render_template("signin.html",message="Wrong password",form=form)
+        else:
+            session["USERNAME"] = session_user["full_name"]
+            return redirect(url_for('home'))
+
+    return render_template("signin.html",form=form)
+
+@app.route("/signout")
+def signout():
+    session.pop("USERNAME",None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, EqualTo, Length
+
+class SignUpForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(),Email(),Length(min=4,max=25)])
+    password = PasswordField('Password', validators=[InputRequired(),EqualTo('confirm',message='Passwords must match'),Length(min=6,max=35)])
+    confirm = PasswordField('Confirm Password', validators=[Length(min=6,max=35)])
+    submit = SubmitField('Submit')
+
+class SignInForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(),Email(),Length(min=4,max=25)])
+    password = PasswordField('Password', validators=[InputRequired(),Length(min=6,max=35)])
+    submit = SubmitField('Sign In')
